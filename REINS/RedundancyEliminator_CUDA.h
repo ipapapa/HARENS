f16c458b90b67c2aec9e199f41570339442cd58f
KeluDiao <diaokelu@gmail.com>
2015-04-14 01:53:39 -0700

    https://github.com/ipapapa/REinNS.git

--- REINS/RedundancyEliminator_CUDA.h
+++ REINS/RedundancyEliminator_CUDA.h
@@ -11,7 +11,7 @@
 #include <openssl/sha.h>
 #include <algorithm>
 #include "RabinHash.h"
-#include "CircularHash.h"
+#include "CircularHash.h" 
 #include "CircularHashPool.h"
 #include "Definition.h"
 
