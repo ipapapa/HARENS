be54c8ac420c33b20a6cce1b1b22128846f40e29
Kelu Diao <diaokelu@gmail.com>
2015-11-25 18:07:49 -0500

    Auto test

---
+++ tests/runtest.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "NaiveCpp.h"
+#include "CppPipeline.h"
+#include "CudaAcceleratedAlg.h"
+#include "Harens.h"
+#include "HashCollisionTest.h"
+
+int main() {
+	IO::input_file_name = "SampleInput.txt";
+	
+}
